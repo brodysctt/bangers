01d9a43907bfebf5d661440429ac0dd1b1ff9cec
bscott4 <brodyscott12+dev@gmail.com>
2021-09-29 21:35:47 -0700

    firebase init

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs2-gbC6ZCWZ_ITPHnNt1zCu3Fn9RsH_Q",
+  authDomain: "bangers-only.firebaseapp.com",
+  projectId: "bangers-only",
+  storageBucket: "bangers-only.appspot.com",
+  messagingSenderId: "330296551516",
+  appId: "1:330296551516:web:ed34bffe81fb8b72695e99",
+  measurementId: "G-47ZKJMPMET",
+};
+
+const app = initializeApp(firebaseConfig);
